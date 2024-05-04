4a72c9e6c1624a6c51689a21846fefe3eae6638a
Yevhenii Baranov <jask.b@outlook.com>
2024-05-04 19:20:03 +0200

    task1 completed

--- task_1/main.py
+++ task_1/main.py
@@ -2,25 +2,23 @@
 def caching_fibonacci() -> int:
     """Calculating fibonacci numbers and saving them to cache. Returning
     values from cache in case of need.
-
-    Args:
-        n (int): number
-
-    Returns:
-        int: number
     """
     cache = {0: 0, 1: 1}
     def fibonacci(n: int) -> int:
         if n in cache:
+            print('return from cache')
             return cache[n]
         cache[n] = fibonacci(n-2) + fibonacci(n-1)
+        print('return calculated')
         return cache[n]
     return fibonacci
 
 
 if __name__ == "__main__":
     fib = caching_fibonacci()
-    print(fib(5))
-    print(fib(5))
-    print(fib(10))
-    print(fib(15))
+    print(f'Result: {fib(0)}')
+    print(f'Result: {fib(1)}')
+    print(f'Result: {fib(10)}')
+    print(f'Result: {fib(10)}')
+    print(f'Result: {fib(15)}')
+    print(f'Result: {fib(15)}')
